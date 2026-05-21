b116c0e67d73d8cda456af60f467693022c39492
Maxx909 <maxterya21@gmail.com>
2026-05-21 18:01:43 +0300

    Add randomization for Apple position
    Import random module and constants for apple position.

--- apple.py
+++ apple.py
@@ -1,3 +1,12 @@
+import random
+from constants import CELLX, CELLY
+
 class Apple:
     def __init__(self):
-        
+        self.x = 0
+        self.y = 0
+        self.randomize()
+
+    def randomize(self):
+        self.x = random.randint(0, CELLX - 1)
+        self.y = random.randint(0, CELLY - 1)
